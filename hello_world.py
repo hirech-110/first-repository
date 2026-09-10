52539a7165d67f8e043967897aeae8a9893d9941
hirech-110 <hirech110@gmail.com>
2026-09-10 16:32:44 +0900

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,7 @@
-def print_hello():
-    print('hello world!!!')
-if __name__=='__main__':
-    print_hello()
+def add(x, y):
+a = x + y
+return a
+def print_KIT():
+print(“KIT")
+if __name__ == '__main__’:print_KIT()
+print( print_hello(1,2) )
